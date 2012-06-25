50cd9a1e3c9aa37646d8c6b4af525280a31e5376
slava.m <viachaslau.markevich@active.by>
2012-06-25 12:11:57 +0300

    add method missing and indexer methods in api:base

--- lib/cloud/cloudstack/api/base.rb
+++ lib/cloud/cloudstack/api/base.rb
@@ -1,5 +1,16 @@
 module Bosh::CloudStackCloud::Api
   class Base
+    def initialize data
+      @data = data
+    end
     
+    def [] key
+      @data[key.to_s]
+    end
+
+    def method_missing(method_name, *args, &block) 
+      @data[method_name.to_s]
+    end
+
   end
 end
