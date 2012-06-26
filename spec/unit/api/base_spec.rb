46b735be76cb11eb0dda303a531ee209436ade78
slava.m <viachaslau.markevich@active.by>
2012-06-26 12:21:03 +0300

    remove unnecessary connection in base_spec

--- spec/unit/api/base_spec.rb
+++ spec/unit/api/base_spec.rb
@@ -2,16 +2,16 @@ require 'spec_helper'
 include Bosh::CloudStackCloud
 describe Api::Base do
   let(:subject){Api::Base}
-  before do
-    create_connection_to_cloudstack
-  end
+
   describe '#method_missing' do
     let(:item){subject.new({:param => :value})}
+
     it 'return data by call instance method' do
       item.param.should == :value
     end
     it 'return data by call indexer' do
       item[:param].should == :value
     end
+    
   end
 end
