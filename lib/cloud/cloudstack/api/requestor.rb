9813c9fad613703bcf078b8aac0137dff53ef1bb
slava.m <viachaslau.markevich@active.by>
2012-06-26 15:14:59 +0300

    implement requestor for list

--- lib/cloud/cloudstack/api/requestor.rb
+++ lib/cloud/cloudstack/api/requestor.rb
@@ -1,7 +1,28 @@
-module Bosh::CloudStackCloud::Api
-  class Requestor
-    def self.request request_name
+module Bosh::CloudStackCloud
+  class Api::FogRequestor
+    class << self
+      def list request_name
+        response = request "list_#{request_name}"
+        parse_list response
+      end
 
+
+      private
+
+      def request request_name
+        set_connection
+        @connection.send(request_name.to_sym)
+      end
+
+      def parse_list data
+        # {"listvirtualmachinesresponse" => {"count" => "", "virtualmachine" => ""}
+
+        data.values[0].values[1]
+      end
+
+      def set_connection
+        @connection ||= Api.connection
+      end
     end
   end
 end
