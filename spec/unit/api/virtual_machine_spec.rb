510b90b193d9ddba154c754d2845d22d5ed0ab9a
slava.m <viachaslau.markevich@active.by>
2012-06-29 11:52:45 +0300

    add test for fog request in deploy virtual machine

--- spec/unit/api/virtual_machine_spec.rb
+++ spec/unit/api/virtual_machine_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 include Bosh::CloudStackCloud::Api
 describe VirtualMachine do
   let(:subject){VirtualMachine}
+  let(:empty_fog_response){{'fogresponse' => {}}}
   before do
     create_connection_to_cloudstack
   end
@@ -29,6 +30,11 @@ describe VirtualMachine do
         'templateid' => '8a31cf9c-f248-0588-256e-9dbf58785216',
         'serviceofferingid' => '4437ac6c-9fe3-477a-57ec-60a5a45896a4'
         }}
+      it 'send request to fog' do
+        Api.connection.should_receive(:deploy_virtual_machine).with(deploy_parameters).and_return empty_fog_response
+        subject.deploy deploy_parameters
+      end
+
       it 'return a vm instance' do
         subject.deploy(deploy_parameters).should be_instance_of VirtualMachine
       end
